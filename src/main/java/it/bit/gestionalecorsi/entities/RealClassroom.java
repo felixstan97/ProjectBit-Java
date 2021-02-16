20f2552e28511805e6353a5f541a14689c2b39c0
andmora <and.morabito@gmail.com>
2021-02-16 09:16:59 +0100

    typo

--- src/main/java/it/bit/gestionalecorsi/entities/RealClassroom.java
+++ src/main/java/it/bit/gestionalecorsi/entities/RealClassroom.java
@@ -2,18 +2,18 @@ package it.bit.gestionalecorsi.entities;
 
 public class RealClassroom extends Classroom{
 
-    public enum EQUIPMENTS {PC, PROJECTOR, NOTHING, FULL}
+    public enum EQUIPMENT {PC, PROJECTOR, NOTHING, FULL}
 
-    private EQUIPMENTS tech;
+    private EQUIPMENT tech;
 
     @Override
     public boolean hasComputer() {
-        return tech == EQUIPMENTS.PC || tech == EQUIPMENTS.FULL;
+        return tech == EQUIPMENT.PC || tech == EQUIPMENT.FULL;
     }
 
     @Override
     public boolean hasProjectors() {
-        return tech == EQUIPMENTS.PROJECTOR || tech == EQUIPMENTS.FULL;
+        return tech == EQUIPMENT.PROJECTOR || tech == EQUIPMENT.FULL;
     }
 
 }
